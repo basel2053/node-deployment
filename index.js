2aa4aa69c887774cab52185dc51150b25ba9dc63
basel2053 <baselsalah2053@gmail.com>
2024-08-09 03:48:17 +0300

    add users route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send("This is users tab");
+});
+
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
